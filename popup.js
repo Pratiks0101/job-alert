f02145551a5a0e0169906b4a68601c1e25bec113
Pratik <sharmapratik789@gmail.com>
2026-07-06 23:40:08 +0530

    implemented localStorage

--- popup.js
+++ popup.js
@@ -3,10 +3,12 @@ const savebtn = document.getElementById("save-button")
 const error = document.getElementById("error")
 
 savebtn.addEventListener("click", ()=> {
+    const errMsg = error.innerText = "Please write atleast one job title"
     if(jobs.value.trim() === ""){
-        console.log(error.innerText="Please write atleast one job title")
+        console.log(errMsg)
         return
     }else{
-        console.log(jobs.value.trim())
+        console.log(error.innerText= "")
+        localStorage.setItem("job titles", jobs.value.trim())
     }
 })
